c2a2e28438743e8066723ebbf9300b16dbfc450e
nombersDev <98760010+nombersDev@users.noreply.github.com>
2022-02-14 13:06:33 +0100

    Update inventoryFunctions.tsx

--- src/renderer/components/content/shared/inventoryFunctions.tsx
+++ src/renderer/components/content/shared/inventoryFunctions.tsx
@@ -280,7 +280,7 @@ export async function sortDataFunction(sortValue, inventory) {
 }
 
 export async function downloadReport(storageData) {
-  let csvContent = "Item Name,Item Custom Name, Item Moveable, Storage Name, Tradehold, Category, Combined QTY, Item Wear Name, Item Paint Wear,Item Has Stickers,Stickers\n";
+  let csvContent = "Item Name,Item Custom Name, Item Moveable, Storage Name, Tradehold, Category, Combined QTY, Item Wear Name, Item Paint Wear,Item Has Stickers/Patches,Stickers\n";
   var csv = storageData.map(function(d){
     let storageName = d.storage_name
     if (storageName == undefined) {
